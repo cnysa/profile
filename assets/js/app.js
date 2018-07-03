507953ebbd9263515a384b52ac5070557c9ecb37
unknown <quyenht@COD.hcos.vn>
2018-07-03 14:45:43 +0700

    push at wwork

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,22 @@ let app =  angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     this.constructor = () => {
         $scope.test();
-        $scope.getLstSkills();
+        $scope.getkeyword();
     }
     $scope.test = function () {
         console.log("quyendeptrai");
     }
-    $scope.getLstSkills = function () {
+    $scope.getkeyword = function () {
         $http.get("http://cnysa.github.io/profile/assets/data/Vi/keyword.json").then(function (d) {
-            console.log(d.data);
             $scope.thongtin = d.data;
             console.log($scope.thongtin)
-    });
+        });
+    }
+    $scope.getcontent = function () {
+        $http.get("http://cnysa.github.io/profile/assets/data/Vi/content.json").then(function (f) {
+            $scope.aboutme = f.data.aboutme;
+            console.log($scope.aboutme)
+        });
     }
     this.constructor();
 })
