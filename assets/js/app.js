d737a434016a8ffa116bfea4243d2f0dd733343f
unknown <quyenht@COD.hcos.vn>
2018-07-03 16:52:17 +0700

    sac

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,21 @@
 let app =  angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     this.constructor = () => {
-        $scope.getkeyword();
-        $scope.Vietnamese();
+        $scope.getvikeyword();
     }
-    $scope.getkeyword = function () {
-        $http.get("https://cnysa.github.io/profile/assets/data/Vi/keyword.json").then(function (d) {
+    $scope.getvikeyword = function () {
+        $http.get("https://github.com/cnysa/profile/blob/master/assets/data/Vi/keyword.json").then(function (d) {
             $scope.thongtin = d.data;
-            console.log($scope.thongtin)
-        });
-    }
-    $scope.english = function (){
-        $http.get("https://cnysa.github.io/profile/assets/data/eng/content.json").then(function (f) {
-            $scope.about = f.data.aboutme;
-            $scope.skill = f.data.skill;
-            $scope.info = f.data.infomation;
-            $scope.edu = f.data.education;
-            console.log($scope.edu)
+            console.log($scope.thongtin);
+            $scope.Vietnamese();
         });
     }
     $scope.Vietnamese = function () {
-        $http.get("https://cnysa.github.io/profile/assets/data/Vi/content.json").then(function (f) {
+        $http.get("https://github.com/cnysa/profile/blob/master/assets/data/Vi/content.json").then(function (f) {
             $scope.about = f.data.aboutme;
             $scope.skill = f.data.skill;
             $scope.info = f.data.infomation;
             $scope.edu = f.data.education;
-            console.log($scope.edu)
         });
     }
     this.constructor();
