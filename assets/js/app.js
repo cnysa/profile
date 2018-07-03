60a00aa6e6cf575b38c601c2600cb5fe1422e109
unknown <quyenht@COD.hcos.vn>
2018-07-03 15:28:52 +0700

    edit at work

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ app.controller('myCtrl', function ($scope, $http) {
     this.constructor = () => {
         $scope.test();
         $scope.getkeyword();
+        $scope.getcontent();
     }
     $scope.test = function () {
         console.log("quyendeptrai");
@@ -15,8 +16,11 @@ app.controller('myCtrl', function ($scope, $http) {
     }
     $scope.getcontent = function () {
         $http.get("http://cnysa.github.io/profile/assets/data/Vi/content.json").then(function (f) {
-            $scope.aboutme = f.data.aboutme;
-            console.log($scope.aboutme)
+            $scope.about = f.data.aboutme;
+            $scope.skill = f.data.skill;
+            $scope.info = f.data.infomation;
+            $scope.edu = f.data.education;
+            console.log($scope.edu)
         });
     }
     this.constructor();
